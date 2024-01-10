c4de1e688f4566c1c0471d0ac075d28146d079d7
Aaron Y <aiyota@outlook.com>
2024-01-10 06:57:40 -0800

    Add search-pdf component to Angular app.

--- src/PdfApp.Presentation.Angular/ClientApp/src/app/app.module.ts
+++ src/PdfApp.Presentation.Angular/ClientApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPdfComponent } from './search-pdf/search-pdf.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPdfComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/PdfApp.Presentation.Angular/ClientApp/src/app/search-pdf/search-pdf.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-pdf',
+  templateUrl: './search-pdf.component.html',
+  styleUrls: ['./search-pdf.component.scss']
+})
+export class SearchPdfComponent {
+
+}
